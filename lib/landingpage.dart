bbb73a1ef8552fe524f52ae684904b16bcc90c3d
DelXN-shivam <shivamkhule.delxn@gmail.com>
2025-02-04 12:56:42 +0530

    Header Updates Pull

--- lib/landingpage.dart
+++ lib/landingpage.dart
@@ -11,132 +11,294 @@ class Landingpage extends StatefulWidget {
 class _LandingpageState extends State<Landingpage> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.only(top: 28),
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/image.png",
-              fit: BoxFit.fitWidth,
-            ),
-            Container(
-              height: 440,
-              width: 380,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/image.png",
+            width: size.width,
+            height: size.height * 0.45,
+            fit: BoxFit.contain,
+          ),
+          Expanded(
+            child: SizedBox(
+              height: size.height * 0.4,
+              width: size.width * 0.9,
               child: Stack(
                 children: [
                   Positioned(
-                      top: 50,
-                      left: 30,
-                      child: Container(
-                        height: 380,
-                        width: 330,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.grey,
-                        ),
-                      )),
-                  Positioned(
-                      top: 65,
-                      left: 55,
-                      child: Container(
-                        width: 290,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset("assets/Broccoli.png"),
-                            Image.asset("assets/Pineapple.png"),
-                          ],
-                        ),
-                      )),
-                  Positioned(
-                      bottom: 35,
-                      left: 55,
-                      child: Container(
-                        width: 290,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.asset("assets/Strawberry.png"),
-                            Image.asset("assets/Carrot.png"),
-                          ],
-                        ),
-                      )),
-                  Positioned(
-                    top: 0,
-                    left: 140,
+                    top: size.height * 0.06,
+                    left: size.width * 0.08,
                     child: Container(
-                      height: 100,
-                      width: 100,
-                      child: Center(
-                        child: Image.asset("assets/Healthy food.png"),
-                      ),
+                      height: size.height * 0.41,
+                      width: size.width * 0.8,
                       decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(70)),
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
                   Positioned(
-                    top: 130,
-                    left: 70,
+                    top: size.height * 0.07,
+                    left: size.width * 0.15,
+                    child: SizedBox(
+                      width: size.width * 0.7,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset("assets/Broccoli.png"),
+                          Image.asset("assets/Pineapple.png"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: size.height * 0.05,
+                    left: size.width * 0.15,
+                    child: SizedBox(
+                      width: size.width * 0.7,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset("assets/Strawberry.png"),
+                          Image.asset("assets/Carrot.png"),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: size.height * 0.01,
+                    left: size.width * 0.4,
                     child: Container(
-                      width: 240,
+                      height: size.height * 0.12,
+                      width: size.width * 0.25,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(70),
+                      ),
+                      child: Center(
+                        child: Image.asset("assets/Healthy food.png"),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: size.height * 0.15,
+                    left: size.width * 0.2,
+                    child: SizedBox(
+                      width: size.width * 0.6,
                       child: Column(
                         children: [
                           Text(
                             "Quick Delivery At Your Door Step",
                             style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.w600),
+                              fontSize: size.width * 0.06,
+                              fontWeight: FontWeight.w600,
+                            ),
                             textAlign: TextAlign.center,
                           ),
                           SizedBox(
-                            height: 20,
+                            height: size.height * 0.02,
                           ),
                           Text(
                             "Your Parcel , Our Priority",
                             style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.w600),
+                              fontSize: size.width * 0.04,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                           SizedBox(
-                            height: 40,
+                            height: size.height * 0.04,
                           ),
                           GestureDetector(
                             onTap: () {
                               Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => LoginPage()));
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ),
+                              );
                             },
                             child: Container(
-                              height: 55,
-                              width: 180,
+                              height: size.height * 0.07,
+                              width: size.width * 0.45,
                               decoration: BoxDecoration(
-                                  color: const Color.fromARGB(255, 27, 102, 29),
-                                  borderRadius: BorderRadius.circular(20)),
+                                color: const Color.fromARGB(255, 27, 102, 29),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
                               child: Center(
                                 child: Text(
                                   "Get Started",
                                   style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: 18),
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: size.width * 0.05,
+                                  ),
                                 ),
                               ),
                             ),
-                          )
+                          ),
                         ],
                       ),
                     ),
-                  )
+                  ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+// import 'package:ecom/authentication/login.dart';
+// import 'package:flutter/material.dart';
+
+// class Landingpage extends StatefulWidget {
+//   const Landingpage({super.key});
+
+//   @override
+//   State<Landingpage> createState() => _LandingpageState();
+// }
+
+// class _LandingpageState extends State<Landingpage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Container(
+//         padding: EdgeInsets.only(top: 28),
+//         height: MediaQuery.of(context).size.height,
+//         width: MediaQuery.of(context).size.width,
+//         child: Column(
+//           children: [
+//             Image.asset(
+//               "assets/image.png",
+//               width: MediaQuery.of(context).size.width,
+//               height: MediaQuery.of(context).size.height * 0.45,
+//               fit: BoxFit.contain,
+//             ),
+//             Expanded(
+//               child: SizedBox(
+//                 height: 400,
+//                 width: 380,
+//                 child: Stack(
+//                   children: [
+//                     Positioned(
+//                         top: 50,
+//                         left: 30,
+//                         child: Container(
+//                           height: 380,
+//                           width: 330,
+//                           decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(20),
+//                             color: Colors.grey,
+//                           ),
+//                         )),
+//                     Positioned(
+//                         top: 65,
+//                         left: 55,
+//                         child: Container(
+//                           width: 290,
+//                           child: Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                             children: [
+//                               Image.asset("assets/Broccoli.png"),
+//                               Image.asset("assets/Pineapple.png"),
+//                             ],
+//                           ),
+//                         )),
+//                     Positioned(
+//                         bottom: 35,
+//                         left: 55,
+//                         child: Container(
+//                           width: 290,
+//                           child: Row(
+//                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                             children: [
+//                               Image.asset("assets/Strawberry.png"),
+//                               Image.asset("assets/Carrot.png"),
+//                             ],
+//                           ),
+//                         )),
+//                     Positioned(
+//                       top: 0,
+//                       left: 140,
+//                       child: Container(
+//                         height: 100,
+//                         width: 100,
+//                         decoration: BoxDecoration(
+//                             color: Colors.white,
+//                             borderRadius: BorderRadius.circular(70)),
+//                         child: Center(
+//                           child: Image.asset("assets/Healthy food.png"),
+//                         ),
+//                       ),
+//                     ),
+//                     Positioned(
+//                       top: 130,
+//                       left: 70,
+//                       child: Container(
+//                         width: 240,
+//                         child: Column(
+//                           children: [
+//                             Text(
+//                               "Quick Delivery At Your Door Step",
+//                               style: TextStyle(
+//                                   fontSize: 30, fontWeight: FontWeight.w600),
+//                               textAlign: TextAlign.center,
+//                             ),
+//                             SizedBox(
+//                               height: 20,
+//                             ),
+//                             Text(
+//                               "Your Parcel , Our Priority",
+//                               style: TextStyle(
+//                                   fontSize: 16, fontWeight: FontWeight.w600),
+//                             ),
+//                             SizedBox(
+//                               height: 40,
+//                             ),
+//                             GestureDetector(
+//                               onTap: () {
+//                                 Navigator.push(
+//                                     context,
+//                                     MaterialPageRoute(
+//                                         builder: (context) => LoginPage()));
+//                               },
+//                               child: Container(
+//                                 height: 55,
+//                                 width: 180,
+//                                 decoration: BoxDecoration(
+//                                     color:
+//                                         const Color.fromARGB(255, 27, 102, 29),
+//                                     borderRadius: BorderRadius.circular(20)),
+//                                 child: Center(
+//                                   child: Text(
+//                                     "Get Started",
+//                                     style: TextStyle(
+//                                         color: Colors.white,
+//                                         fontWeight: FontWeight.w600,
+//                                         fontSize: 18),
+//                                   ),
+//                                 ),
+//                               ),
+//                             )
+//                           ],
+//                         ),
+//                       ),
+//                     )
+//                   ],
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,66 +1,5 @@
-// import 'package:ecom/homepage.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-
-// class Splashscreen extends StatefulWidget {
-//   const Splashscreen({super.key});
-
-//   @override
-//   State<Splashscreen> createState() => _SplashscreenState();
-// }
-
-// class _SplashscreenState extends State<Splashscreen> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     _navigateToHomeScreen();
-//   }
-
-//   _navigateToHomeScreen() {
-//     Future.delayed(Duration(seconds: 5), () {
-//       Navigator.pushReplacement(
-//         context,
-//         MaterialPageRoute(builder: (context) => Homepage()),
-//       );
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         children: [
-//           SizedBox(
-//             height: MediaQuery.of(context).size.height,
-//             width: MediaQuery.of(context).size.width,
-//             child: Image.asset(
-//               "assets/splashimage.jpeg",
-//               fit: BoxFit.cover,
-//             ),
-//           ),
-//           Positioned(
-//             top: MediaQuery.of(context).size.height / 2.1,
-//             right: MediaQuery.of(context).size.width / 3.5,
-//             child: Text(
-//               "My Shop",
-//               style: GoogleFonts.dancingScript(
-//                 textStyle: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 48,
-//                   fontWeight: FontWeight.w900,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:ecom/landingpage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
@@ -94,40 +33,18 @@ class _State extends State<Splashscreen> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Colors.amber,
-        child: Stack(children: [
-          Container(
+        child: Stack(
+          children: [
+            Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
               child: Image.asset(
                 "assets/splashimage.jpeg",
                 fit: BoxFit.fitHeight,
-              )),
-          // Positioned(
-          //     bottom: 100,
-          //     left: 120,
-          //     child: Text(
-          //       "Secure Guard",
-          //       style: TextStyle(
-          //           fontSize: 36,
-          //           fontWeight: FontWeight.w700,
-          //           color: Colors.white),
-          //     ))
-
-          //  Positioned(
-          //   top: MediaQuery.of(context).size.height / 2.1,
-          //   right: MediaQuery.of(context).size.width / 3.5,
-          //   child: Text(
-          //     "My Shop",
-          //     style: GoogleFonts.dancingScript(
-          //       textStyle: TextStyle(
-          //         color: Colors.white,
-          //         fontSize: 48,
-          //         fontWeight: FontWeight.w900,
-          //       ),
-          //     ),
-          //   ),
-          //  ),
-        ]),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
