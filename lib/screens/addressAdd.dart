5d789e908cac2ea1a1d961d1e0f5b1371ad78f62
Prathamdelxn <godageprathmesh.delxn@gmail.com>
2025-02-06 16:51:33 +0530

    new update

---
+++ lib/screens/addressAdd.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddressPage extends StatefulWidget {
+  const AddressPage({super.key});
+
+  @override
+  State<AddressPage> createState() => _AddressPageState();
+}
+
+class _AddressPageState extends State<AddressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Add Address Details"),
+      ),
+    );
+  }
+}
